e12b75bfde264fd0a8a12a421c5b9d2d512a6807
Souvik <souvik13109@gmail.com>
2025-03-27 20:42:23 +0530

    Refactor server initialization and add MongoDB connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ import express from 'express';
 const app = express();
 
 
+import connectDB from './src/db/index.js';
+import dotenv from 'dotenv';
+dotenv.config({
+    path:'./.env'
+});
+connectDB();
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`Server running at:`, PORT)
