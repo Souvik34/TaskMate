72aa83c61f1959aa3c87249b0ab552359404ffbb
Souvik <souvik13109@gmail.com>
2025-03-27 20:43:10 +0530

    Reorganize database connection import in server index file

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 const app = express();
-
-
-import connectDB from './src/db/index.js';
 import dotenv from 'dotenv';
 dotenv.config({
     path:'./.env'
 });
+
+
+// Database connection
+import connectDB from './src/db/index.js';
 connectDB();
+
+
+
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`Server running at:`, PORT)
