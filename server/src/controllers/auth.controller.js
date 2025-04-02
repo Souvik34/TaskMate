2c2dac5a4ad3e578aa75ddf1390b7db863b40222
Souvik <souvik13109@gmail.com>
2025-04-03 00:45:30 +0530

    Signup method created

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,9 +1,48 @@
 import { User } from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcryptjs";
 
 
-const signup= async(req, res, next)=>{
+export const signup= async(req, res, next)=>{
     try {
-        
+        const {
+            username,
+            email,
+            password
+        }= req.body;
+
+        // Check if user already exists
+        const isValidator =await User.findOne({email});
+        if(isValidator)
+        {
+            return next(errorHandler(400, "User already exists"))
+        }
+
+        // Hash password
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        // Create new user
+
+        const newUser= await User.create({
+            username,
+            email,
+            password : hashedPassword
+
+        })
+        // Send response
+         
+        res.status(201).json({
+            success: true,
+            message: "User created successfully !",
+            data: {
+                username: newUser.username,
+                email: newUser.email,
+                timestamp: newUser.createdAt,
+                id: newUser._id
+            }
+
+        })
+
     } catch (error) {
         next(error)
     }
