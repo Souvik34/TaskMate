dd5860c2fc6f2a804ab5e8b29af03c48712f8785
Souvik <souvik13109@gmail.com>
2025-04-03 01:06:28 +0530

    Implement authentication routes and error handling; update user model and add bcryptjs dependency

--- server/index.js
+++ server/index.js
@@ -9,10 +9,34 @@ dotenv.config({
 // Database connection
 import connectDB from './src/db/index.js';
 connectDB();
-
+app.use(express.json())
 
 // PORT
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`Server running at:`, PORT)
 })
+
+
+
+// import routes
+
+import authRouter from './src/routes/auth.route.js';
+app.use("/api/v1/auth", authRouter)
+
+
+
+
+//error handling
+app.use((err, req, res, next) =>
+    {
+        const statusCode = err.statusCode ||500
+        const message= err.message || "Internal server error"
+    
+        return res.status(statusCode).json({
+            success:false,
+            statusCode,
+            message,
+        })
+    
+    })
