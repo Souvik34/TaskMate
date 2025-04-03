e84a1f14493b7831d22cc9d2548514d7ecdbf3b2
Souvik <souvik13109@gmail.com>
2025-04-03 20:23:12 +0530

    Refactor authentication controller and routes; export signin method and update route definitions

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -48,19 +48,19 @@ export const signup= async(req, res, next)=>{
 }
 
 
-const signin = async(req, res, next)=>{
+export const signin = async(req, res, next)=>{
     const { email, password} = req.body;
 
     try {
         const validUser = await User.findOne({email});
 
-        if(!validuser)
+        if(!validUser)
         {
             return next(errorHandler(404, "User not found"))
         }
         // Check password
 
-        const validPassword = await comparePassword(password);
+        const validPassword = await validUser.comparePassword(password);
 
         if(!validPassword)
         {
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
-const router = express.Router();
+import {Router} from 'express';
+import { signup , signin} from '../controllers/auth.controller.js';
+const router = Router();
 
-router.post('/signup', signup);
+router.route('/signup').post(signup);
+router.route('/signin').post(signin);
 
 
 export default router;
