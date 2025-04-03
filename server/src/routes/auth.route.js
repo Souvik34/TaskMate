5b54c7f4840afd695910d915e0f9699ee955c638
Souvik <souvik13109@gmail.com>
2025-04-03 21:00:41 +0530

    verifyUser removed from routes

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,6 +1,5 @@
 import {Router} from 'express';
 import { signup , signin, signout} from '../controllers/auth.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
 const router = Router();
 
 router.route('/signup').post(signup);
